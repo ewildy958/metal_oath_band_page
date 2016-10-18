21a4d2836a8385a4b332265b61107057c7d766f9
Elisa Wildy <elisa.wildy@my.simpson.edu>
2016-10-18 11:52:31 -0500

    deploy script added

--- public_html/deploy_script.php
+++ public_html/deploy_script.php
@@ -15,21 +15,4 @@ echo $output;
 		
 		</pre>
 	</body>
-</html><!DOCTYPE html>
-
-<html lang="en">
-	<head>
-		<title>Deployment Script</title>
-	</head>
-	<body>
-	<p>Deploy script</p>
-		<pre>
-		
-<?php
-$output = shell_exec("cd ..;git pull 2>&1");
-echo $output;
-?>
-		
-		</pre>
-	</body>
 </html>
