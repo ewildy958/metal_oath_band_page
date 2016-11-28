2ecdc4df35c2b28937d5ff8d164b820cced9266d
Elisa Wildy <elisa.wildy@my.simpson.edu>
2016-11-28 15:56:13 -0600

    changed client id

--- public_html/handle_login.php
+++ public_html/handle_login.php
@@ -6,7 +6,7 @@ $r = curl_exec($c);
 curl_close($c);
 $d = json_decode($r);
  
-if ($d->aud != 'YOUR-CLIENT-ID') {
+if ($d->aud != 'amzn1.application-oa2-client.e8a58dbec58b4556be90bada510a92f0') {
   // the access token does not belong to us
   header('HTTP/1.1 404 Not Found');
   echo 'Page not found';
