b3c5ce0fd83d87b22e3c0353c99f6a9da16c978d
Elisa Wildy <elisa.wildy@my.simpson.edu>
2016-11-28 16:04:19 -0600

    no message

--- public_html/log_in.php
+++ public_html/log_in.php
@@ -13,22 +13,7 @@
 </head>
 
 <body>
-	<div id="amazon-root"></div>
-	<script type="text/javascript">
-
-		window.onAmazonLoginReady = function() {
-			amazon.Login.setClientID('amzn1.application-oa2-client.e8a58dbec58b4556be90bada510a92f0');
-		};
-
-		(function(d) {
-			var a = d.createElement('script'); a.type= 'text/javascript';
-			a.async = true; a.id = 'amazon-login-sdk';
-			a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
-			d.getElementById('amazon-root').appendChild(a);
-		})(document);
-	</script>
-
-    <header>
+	<header>
 	<img id="logo" src="img/metal_oath.png" alt="Metal Oath">
     </header>
   
@@ -46,6 +31,21 @@
 	</form>
 	<br>
 
+	<div id="amazon-root"></div>
+	<script type="text/javascript">
+
+		window.onAmazonLoginReady = function() {
+			amazon.Login.setClientID('amzn1.application-oa2-client.e8a58dbec58b4556be90bada510a92f0');
+		};
+
+		(function(d) {
+			var a = d.createElement('script'); a.type= 'text/javascript';
+			a.async = true; a.id = 'amazon-login-sdk';
+			a.src = 'https://api-cdn.amazon.com/sdk/login1.js';
+			d.getElementById('amazon-root').appendChild(a);
+		})(document);
+	</script>
+
 	<a href="#" id="LoginWithAmazon">
 		<img border="0" alt="Login with Amazon"
 		src="https://images-na.ssl-images-amazon.com/images/G/01/lwa/btnLWA_gold_156x32.png"
@@ -55,7 +55,7 @@
 	<script type="text/javascript">
 		document.getElementById('LoginWithAmazon').onclick = function() {
 			options = { scope : 'profile' };
-			amazon.Login.authorize(options, 'http://ec2-35-160-178-88.us-west-2.compute.amazonaws.com/log_in.php/handle_login.php');
+			amazon.Login.authorize(options, 'http://ec2-35-160-178-88.us-west-2.compute.amazonaws.com/log_in.php');
 			return false;
 		};
 	</script>
